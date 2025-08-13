b0640a09a26e95d7a8c1005b3951ccea13c882d1
Marly <marlysaliba0@gmail.com>
2025-08-13 14:54:39 -0400

    Nothing to commit merge failed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import Header from './Components/header.jsx'
+import Header from './Components/Header.jsx'
 import Landing from './Components/Landing.jsx'
 import './App.css'
 
